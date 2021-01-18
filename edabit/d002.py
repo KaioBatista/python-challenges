2f90175d6d220fa2e2e742464941b4f4748b049a
Kaio Batista Almeida <kaiobatist@gmail.com>
2021-01-18 01:41:54 -0300

    challenge done

---
+++ edabit/d002.py
@@ -0,0 +1,11 @@
+# Desafio 002:
+#   Return the Remainder from Two Numbers
+# ----------------------------------------------------------------------------
+
+
+def resto_divisao(a: int, b: int) -> int:
+    return a % b
+
+
+if __name__ == '__main__':
+    print(resto_divisao(8, 2))
