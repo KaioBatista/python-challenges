c699997141635156e85aad32d30abf20818d5f7b
Kaio Batista Almeida <kaiobatist@gmail.com>
2021-01-18 01:33:50 -0300

    challange done

---
+++ edabit/d001.py
@@ -0,0 +1,11 @@
+# Desafio 001:
+# Create a function that takes two numbers as arguments and return their sum.
+# ----------------------------------------------------------------------------
+
+
+def soma(a: float, b: float) -> float:
+    return a + b
+
+
+if __name__ == '__main__':
+    print(soma(1, 7))
